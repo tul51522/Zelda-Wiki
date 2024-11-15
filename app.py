04c0b7fe861a6c26ee0a4d5927a68b65da8ec93e
tul51522 <tul51522@temple.edu>
2024-11-15 14:19:57 +0900

    added routes to each main page

--- app.py
+++ app.py
@@ -9,6 +9,22 @@ app.secret_key = "SOME_KEY"
 def index():
     return render_template('index.html', message="index")
 
+@app.route('/maps')
+def maps():
+    return render_template('maps.html')
+
+@app.route('/guides')
+def guides():
+    return render_template('guides.html')
+
+@app.route('/canon')
+def canon():
+    return render_template('canon.html')
+
+@app.route('/community')
+def community():
+    return render_template('community.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
