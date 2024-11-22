b5ab9466561df29e023f4dd5b2dc6e6a34aeaafd
tul51522 <tul51522@temple.edu>
2024-11-22 15:07:29 +0900

    added user creatable articles

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template, session, redirect, url_for
 import random
 import string
+from flask import Flask, jsonify
+from flask_mysqldb import MySQL
+
+
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = "SOME_KEY"
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '12345678'
+app.config['MYSQL_DB'] = 'zelda_wiki'
+
+mysql = MySQL()
+mysql.init_app(app)
 
 @app.route('/')
 def index():
@@ -25,6 +37,26 @@ def canon():
 def community():
     return render_template('community.html')
 
+@app.route('/newarticle', methods=["GET", "POST"])
+def newarticle():
+    if request.method == "POST":
+       cursor = mysql.connection.cursor()
+       article_subject = request.form.get("asubject")
+       imageurl = request.form.get("iurl") 
+       content = request.form.get("acontent")
+       cursor.execute(''' INSERT INTO articles VALUES(%s,%s, %s, 1)''',(article_subject, imageurl, content))
+       mysql.connection.commit()
+       cursor.close()
+       return redirect(url_for('index'))
+    else:
+        return render_template("newarticle.html")
+
+@app.route('/displayarticle/<name>')
+def displayarticle(name):
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM articles WHERE articlesubject = %s", (name,))
+    articleinfo = cursor.fetchone()
+    return render_template('displayarticle.html', info=articleinfo)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
