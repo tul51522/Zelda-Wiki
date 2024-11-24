2e182ec2e08134127d4dabd7620743008b8730e0
kk <kk@KKMacBook-Pro.local>
2024-11-25 00:26:28 +0900

    new

--- app.py
+++ app.py
@@ -14,17 +14,44 @@ def index():
 def maps():
     return render_template('maps.html')
 
-@app.route('/guides')
-def guides_home():
-    return render_template('guides.html')
+games = [
+        'ocarina_of_time',
+        'majoras_mask',
+        'wind_waker',
+        'twilight_princess',
+        'skyward_sword',
+        'breath_of_the_wild',
+        'tears_of_the_kingdom'
+    ]
 
-@app.route('guides/<name>')
+@app.route('/guides')
+def guides():
+    
+    return render_template('guides.html', games=games, selected_game=None)
+
+@app.route('/guides/<name>')
 def guides_type(name):
-    return render_template('html file', name=name)
+   
+    if name not in games:
+        name = None
+    return render_template('guides.html', games=games, selected_game=name)
+
+canons = [
+    'timeline',
+    'characters',
+    'events',
+    'myths'
+]
 
 @app.route('/canon')
 def canon():
-    return render_template('canon.html')
+    return render_template('canon.html', canons=canons, selected_canon=None)
+
+@app.route('/canon/<canon_name>')
+def canon_type(canon_name):
+    if canon_name not in canons:
+        canon_name=None
+    return render_template('canon.html', canons=canons, selected_canon=canon_name)
 
 @app.route('/community')
 def community():
